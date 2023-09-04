7eb9516ed4ea855eea71b0355ba5159fb1551641
Erudite885 <erudite885@gmail.com>
2023-09-04 01:36:43 +0100

    details page modified

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { JobDetail } from "../components";
-// import { useGetSearchFilterQuery } from '../redux/services/jobSearchApi';
+import React from 'react'
 
-const JobDetails = (currentJob) => {
-  const { job_id: jobID } = currentJob;
+const JobDetails = () => {
   return (
-    <div className="bg-secondary_bg min-h-screen dark:bg-black_BG dark:text-natural_3 py-8">
-      <JobDetail job={currentJob} key={jobID} />
-    </div>
-  );
-};
-export default JobDetails;
+    <div>JobDetails</div>
+  )
+}
+
+export default JobDetails
