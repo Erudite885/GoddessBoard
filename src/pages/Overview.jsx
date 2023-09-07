21542340b629fceeb934d3cd6face9e0262e50e3
Erudite885 <erudite885@gmail.com>
2023-09-07 04:02:05 +0100

    overview page component and styling fixed

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,65 +1,92 @@
 import React from "react";
 import {
   useGetJobsQuery,
-  // useGetJobDetailsQuery,
-  // useGetJobSalaryEstimateQuery,
-  // useGetJobSearchFiltersQuery,
+  useGetJobDetailsQuery,
+  //  useGetJobSalaryEstimateQuery,
+  useGetJobSearchFiltersQuery,
 } from "../services/jobApi";
 import { Link, Navigate } from "react-router-dom";
-// import {JobSearch} from "./index";
+import { JobSearch } from "./index";
 import moment from "moment";
+import { Loader } from "../components";
 
 const Overview = () => {
-  const { data, isFetching } = useGetJobsQuery();
+  const { data, isFetching } = useGetJobsQuery(
+    {
+      searchJob: "Frontend Developer",
+      datePosted: "today",
+      remote: true,
+    },
+    {
+      pollingInterval: 50000,
+    }
+  );
+
+ if (isFetching) {
+   return (
+     <div className="flex flex-col items-center justify-center">
+       <Loader />
+     </div>
+   );
+ }
+
   const jobs = data?.data;
 
-  const specificTime = moment(jobs?.job_posted_at_datetime_utc); // Specific time to compare
-  const oneHourAgo = moment().subtract(1, "hour"); // Calculate the time 1 hour ago
+  console.log(jobs)
 
-  const color = specificTime.isAfter(oneHourAgo) ? "red" : "limegreen"; // Ternary expression to check if specific time is greater than 1 hour ago
+   const sortedJobs = [...jobs].sort((a, b) =>
+     moment(b.job_posted_at_datetime_utc).diff(
+       moment(a.job_posted_at_datetime_utc)
+     )
+  );
+  console.log("sortedjobs", sortedJobs);
+  //  console.log(sortedJobs);
 
-  if (isFetching) {
-    return <p>Please Wait...</p>;
-  }
-  // console.log(data?.data);
+  
+  const specificTime = moment(jobs?.job_posted_at_datetime_utc);
+  const oneHourAgo = moment().subtract(1, "hour");
+  const color = specificTime.isAfter(oneHourAgo) ? "red" : "limegreen";
 
   return (
-    <section className="flex flex-col container w-full py-8 lg:max-w-7xl mx-auto">
+    <section className="flex flex-col container w-full py-8 lg:max-w-7xl mx-auto dark:text-white">
       <div className="flex items-center justify-between mx-6 ss:mx-20 mb-6">
         <h2 className="text-2xl font-bold ">Frontend React Jobs today.</h2>
         <button className="bg-[#ab0b0b] rounded-lg p-1 text-white">
           <Link to="/jobsearch">Show More</Link>
         </button>
       </div>
-      <div
-        onClick={() => {
-          // Navigate({'/job-search'})
-        }}
-        className="flex flex-wrap justify-center items-center p-16">
-        {jobs.slice(0, 4).map((job) => (
-          <div
-            key={job.job_id}
-            className="border rounded-xl w-[280px] mx-4 py-4 px-4 mb-4 border-red-400 "
-          >
-            <div className="flex max-w-xs items-center mb-2 gap-2">
-              <img
-                src={job.employer_logo}
-                alt={job.employer_name}
-                className="w-8 h-8 rounded-full"
-              />
-              <h2 className="font-semibold text-xs">{job.employer_name}</h2>
+      <div className="flex flex-wrap justify-center items-center p-16">
+        {sortedJobs.map((job) => (
+            <div
+              key={job.job_id}
+              className="border rounded-xl w-[580px] py-4 px-2 mb-4 border-red-600 "
+            >
+              <div className="flex max-w-xs items-center mb-2 gap-2">
+                <img
+                  src={job.employer_logo}
+                  alt={job.employer_name}
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col cursor-pointer">
+                  <Link to={`jobs/${job.job_id}`}>
+                    <h2 className="font-semibold text-xs">{job.job_title}</h2>
+                    <h2 className="font-medium mt-1 text-xs">
+                      {job.employer_name}
+                    </h2>
+                  </Link>
+                </div>
+              </div>
+              <h6 className="mb-2 text-slate-500 ">{job.job_publisher}</h6>
+              <p>
+                {job.job_description.length > 300
+                  ? `${job.job_description.substring(0, 200)}...`
+                  : `${job.job_description}`}{" "}
+              </p>
+              <p style={{ color }} className={`py-2 text-sm`}>
+                {moment(job.job_posted_at_datetime_utc).fromNow()}{" "}
+              </p>
             </div>
-            <h6 className="mb-2 text-slate-500 ">{job.job_publisher}</h6>
-            <p>
-              {job.job_description.length > 300
-                ? `${job.job_description.substring(0, 200)}...`
-                : `${job.job_description}`}{" "}
-            </p>
-            <p style={{ color }} className={`py-2 text-sm`}>
-              {moment(job.job_posted_at_datetime_utc).fromNow()}{" "}
-            </p>
-          </div>
-        ))}
+          ))}
       </div>
     </section>
   );
