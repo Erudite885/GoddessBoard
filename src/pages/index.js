f4af1837bda8791c7d5fd004b62da48e2815177c
Erudite885 <erudite885@gmail.com>
2023-09-10 04:34:25 +0100

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import Overview from "./Overview";
-import EstimatedSalaries from "./EstimatedSalaries";
+// import EstimatedSalaries from "./EstimatedSalaries";
 import JobSearch from "./JobSearch";
 import JobDetails from "./JobDetails";
 import CompanyDetails from "./CompanyDetails";
 
-export { Overview, EstimatedSalaries, JobSearch, JobDetails, CompanyDetails };
+export {
+    Overview,
+    // EstimatedSalaries,
+    JobSearch,
+    JobDetails,
+    CompanyDetails
+};
