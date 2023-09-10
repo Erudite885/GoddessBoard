2e33c48bb2af77e2b60aa00f8168725bf2ee3008
Erudite885 <erudite885@gmail.com>
2023-09-10 04:37:06 +0100

    listeners setup in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
 import { jobApi } from "../services/jobApi";
 
 export const store = configureStore({
@@ -6,3 +8,4 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([jobApi.middleware]),
 });
+setupListeners(store.dispatch)
